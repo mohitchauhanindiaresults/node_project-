b30a6b72583481742312769347ae0644db61181c
mohitchauhanindiaresults <mohit.chauhan@indiaresults.com>
2024-05-29 12:12:31 +0530

    first commit

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ require('dotenv').config();
 const express = require("express");
 const mongoose = require("mongoose");
 const User = require('./User'); // Import the User model
-const bcrypt = require('bcrypt');
+
 
 const app = express();
 const PORT = process.env.PORT || 8000;
@@ -36,11 +36,11 @@ app.post('/register', async (req, res) => {
             return res.status(400).send('Name and password are required');
         }
 
-        const hashedPassword = await bcrypt.hash(password, 10);
-        const newUser = new User({ name, password: hashedPassword });
+       // const hashedPassword = await bcrypt.hash(password, 10);
+        const newUser = new User({ name, password });
         await newUser.save();
 
-        res.status(201).send('User registered successfully');
+        res.status(200).send('User registered successfully');
     } catch (error) {
         console.error(error);
         res.status(500).send('Server error');
